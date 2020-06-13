00e98cea16af41a8ebfba2ae1783ed955f2dfd81
realprogrammer5000 <me@programmer5000.com>
2020-06-13 13:15:10 -0400

    Add api path

--- index.js
+++ index.js
@@ -1,44 +1,25 @@
-const { spawn } = require('child_process');
-const ffmpeg = require('fluent-ffmpeg');
-const fs = require('fs/promises');
-const os = require('os');
-const path = require('path');
-const crypto = require('crypto');
-const format = (str, obj) => str.replace(/{([\da-zA-Z_-]+)}/g, (match, key) => typeof obj[key] !== 'undefined' ? obj[key] : match);
+const express = require('express');
+const app = express();
 
-const getTmpFilePrefix = () => path.join(os.tmpdir(), crypto.randomBytes(16).toString('hex'));
+const makeVideo = require("./makevideo.js");
 
-const writeMLTFile = async (title) => {
-    const string = (await fs.readFile(path.join(__dirname, 'assets', 'project.template.mlt'))).toString();
-    // https://en.wikipedia.org/wiki/Symlink_race
-    // should be secure enough
-    const renderedVideoPath = getTmpFilePrefix() + '.mp4';
-    const outputFileText = format(string, {title, outFile: renderedVideoPath, basePath: path.join(__dirname, 'assets')});
-    const mltFilePath = getTmpFilePrefix() + '.mlt';
-    await fs.writeFile(mltFilePath, outputFileText);
-    return {mltFilePath, renderedVideoPath};
-};
+const cacheMap = Object.create(null);
 
-const meltIt = path => new Promise(resolve => {
-    spawn('melt', [path]).on('close', () => resolve())
+// noinspection JSUnresolvedFunction
+app.get("/video", async (req, res) => {
+    if(!req.query.text || typeof req.query.text !== "string" || !req.query.text.length || req.query.text.length > 300){
+        return res.status(400).send("Invalid or missing text parameter");
+    }
+
+    const text = req.query.text;
+    if(req.query.text in cacheMap){
+        return res.status(200).sendFile(cacheMap[text]);
+    }
+
+    const path = await makeVideo(text);
+    res.status(200).sendFile(path);
+    cacheMap[text] = path;
 });
 
-const combineClips = (introPath) => new Promise(resolve => {
-    const outPath = getTmpFilePrefix() + '.mp4';
-    ffmpeg(introPath).input(path.join(__dirname, 'assets', 'endscreen.mp4')).on('end', () => resolve(outPath)).on('progress', function(progress) {
-        console.log('Processing: ' + progress.percent + '% done');
-    }).mergeToFile(outPath, os.tmpdir())
-});
 
-(async () => {
-    console.log('writing mlt file');
-    const {mltFilePath, renderedVideoPath} = await writeMLTFile(process.argv[2]);
-    console.log('melting');
-    await meltIt(mltFilePath);
-    await fs.unlink(mltFilePath);
-    console.log('combining');
-    const outPath = await combineClips(renderedVideoPath);
-    console.log('unlinking');
-    await fs.unlink(renderedVideoPath);
-    console.log(outPath);
-})();
+app.listen(process.env.PORT || 5000);
---
+++ makevideo.js
@@ -0,0 +1,46 @@
+const { spawn } = require('child_process');
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs/promises');
+const os = require('os');
+const path = require('path');
+const crypto = require('crypto');
+const format = (str, obj) => str.replace(/{([\da-zA-Z_-]+)}/g, (match, key) => typeof obj[key] !== 'undefined' ? obj[key] : match);
+
+const getTmpFilePrefix = () => path.join(os.tmpdir(), crypto.randomBytes(16).toString('hex'));
+
+const writeMLTFile = async (title) => {
+    const string = (await fs.readFile(path.join(__dirname, 'assets', 'project.template.mlt'))).toString();
+    // https://en.wikipedia.org/wiki/Symlink_race
+    // should be secure enough
+    const renderedVideoPath = getTmpFilePrefix() + '.mp4';
+    const outputFileText = format(string, {title, outFile: renderedVideoPath, basePath: path.join(__dirname, 'assets')});
+    const mltFilePath = getTmpFilePrefix() + '.mlt';
+    await fs.writeFile(mltFilePath, outputFileText);
+    return {mltFilePath, renderedVideoPath};
+};
+
+const meltIt = path => new Promise(resolve => {
+    spawn('melt', [path]).on('close', () => resolve())
+});
+
+const combineClips = (introPath) => new Promise(resolve => {
+    const outPath = getTmpFilePrefix() + '.mp4';
+    ffmpeg(introPath).input(path.join(__dirname, 'assets', 'endscreen.mp4')).on('end', () => resolve(outPath)).on('progress', function(progress) {
+        console.log('Processing: ' + progress.percent + '% done');
+    }).mergeToFile(outPath, os.tmpdir())
+});
+
+const makeVideo = async text => {
+    console.log('writing mlt file');
+    const {mltFilePath, renderedVideoPath} = await writeMLTFile(text);
+    console.log('melting');
+    await meltIt(mltFilePath);
+    await fs.unlink(mltFilePath);
+    console.log('combining');
+    const outPath = await combineClips(renderedVideoPath);
+    console.log('unlinking');
+    await fs.unlink(renderedVideoPath);
+    return outPath;
+};
+
+module.exports = makeVideo;
